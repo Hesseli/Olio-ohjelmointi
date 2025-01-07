9c0aa5da3ad26a7c03de0c8d35329a0d21ccd88b
Jesse <jesse.aittala@hotmail.com>
2025-01-07 13:03:27 +0200

    teht1 arvauspeli tehty

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    srand(time(0));
+
+    int cnum = rand() % 20 + 1;
+    int gnum;
+
+    cout << "Arvaa luku valilta 1-20, niin tarkistetaan onko se oikein" << endl;
+
+    do {
+        cout << "Arvaa lukua: ";
+        cin >> gnum;
+
+        if(gnum < cnum) {
+            cout << "Haetaan suurempaa lukua" << endl;
+        } else if(gnum > cnum) {
+            cout << "Haetaan pienempaa lukua" << endl;
+        } else {
+            cout << "Arvasit oikein!" << endl;
+        }
+    } while(gnum != cnum);
+
     return 0;
 }
